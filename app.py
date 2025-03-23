e7bf7191dbe821d32a6862c3e7080650825b34f0
Geraldo <72437482+Gergerr@users.noreply.github.com>
2025-03-24 04:47:13 +0700

    Fixing imports

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 import pandas as pd
 import sqlite3
+import re
 from rag.indexer import Indexer
 from rag.rag_pipeline import RAGPipeline
 from rag.data_loader import DataLoader
